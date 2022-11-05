9c55aa233024d3966b5df121c7e81a47c0214aff
arezends <artur.rezendesouza@everis.nttdata.com>
2022-11-05 12:33:54 -0300

    inicio implementação do Room ( Entities -> Dao -> DataBase)

--- app/src/main/java/br/com/alura/orgs/model/Produto.kt
+++ app/src/main/java/br/com/alura/orgs/model/Produto.kt
@@ -1,11 +1,16 @@
 package br.com.alura.orgs.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
 
+@Entity
 @Parcelize
 data class Produto(
+        @PrimaryKey(autoGenerate = true)
+        val id: Int = 0,
         val nome: String,
         val descricao: String,
         val valor: BigDecimal,
--- app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ListaProdutosActivity.kt
@@ -3,9 +3,13 @@ package br.com.alura.orgs.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.room.Room
 import br.com.alura.orgs.dao.ProdutosDao
+import br.com.alura.orgs.database.AppDataBase
 import br.com.alura.orgs.databinding.ActivityListaProdutosActivityBinding
+import br.com.alura.orgs.model.Produto
 import br.com.alura.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class ListaProdutosActivity : AppCompatActivity() {
 
@@ -21,11 +25,27 @@ class ListaProdutosActivity : AppCompatActivity() {
         configuraRecyclerView()
         configuraFab()
 
+        val db = Room.databaseBuilder(
+            this,
+            AppDataBase::class.java,
+            "orgs.db"
+        ).allowMainThreadQueries()
+            .build()
+        val produtoDao = db.produtoDao()
+        produtoDao.salva(
+            Produto(
+                nome = "teste nome'",
+                descricao = "teste desc 1",
+                valor = BigDecimal("10.0")
+            )
+        )
+        adapter.atualiza(produtoDao.buscaTodos())
+
     }
 
     override fun onResume() {
         super.onResume()
-        adapter.atualiza(dao.buscaTodos())
+      //  adapter.atualiza(dao.buscaTodos())
     }
 
     private fun configuraFab() {
@@ -35,7 +55,7 @@ class ListaProdutosActivity : AppCompatActivity() {
         }
     }
 
-     private fun vaiParaFormularioProduto() {
+    private fun vaiParaFormularioProduto() {
         val intent = Intent(this, FormularioProdutoActivity::class.java)
         startActivity(intent)
     }
